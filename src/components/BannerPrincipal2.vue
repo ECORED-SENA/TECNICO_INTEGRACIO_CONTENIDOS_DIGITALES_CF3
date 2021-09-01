c44edcbcc5da88e1b3d63d0963577f56f6ae0e8b
Andrea Botello <andrea.botellor@misena.edu.co>
2021-09-01 12:07:46 -0500

    updated

---
+++ src/components/BannerPrincipal2.vue
@@ -0,0 +1,131 @@
+<template lang="pug">
+.banner-principal
+  .container.tarjeta(
+    :style="{'background-image': globalData.fondoBannerPrincipal ? `url(${globalData.fondoBannerPrincipal})` : 'none'}"
+  )
+    .row.banner-principal__row
+      .col-lg-7.col-xxl-5.ps-4.ps-sm-5.py-4.py-sm-5.banner-principal__info
+        .banner-principal__componente
+          h1.mb-0(v-html="globalData.componenteFormativo")
+        .banner-principal__descripcion
+          p.mb-0(v-html="globalData.descripcionCurso")
+        .banner-principal__accion
+          router-link.boton(:to="{name: iniciarLnk.nombreRuta }")
+            span.me-1 Ver más
+            i.fas.fa-angle-right
+
+      .d-none.d-lg-block.col-lg-5.px-0.banner-principal__img
+        .contenedor-imagenes
+          .imagen_flotante.imagen_flotante_1: img(src="@/assets/curso/flotantes1.svg")
+          .imagen_flotante.imagen_flotante_2: img(src="@/assets/curso/flotantes2.svg")
+          img(:src="globalData.imagenBannerPrincipal")
+</template>
+
+<script>
+import mixins from '../components/plantillaMixins'
+export default {
+  name: 'BannerPrincipal',
+  mixins: [mixins],
+  computed: {
+    globalData() {
+      return this.$config.global
+    },
+  },
+}
+</script>
+
+<style lang="sass">
+.banner-principal
+  p, h1, h2, h3, h4, h5, h6
+    color: $color-banner-text
+
+  .tarjeta
+    background-color: $color-banner-fondo
+    background-size: cover
+    background-position: center
+
+  &__info
+    display: flex
+    flex-direction: column
+    justify-content: center
+
+  &__programa
+    display: flex
+    align-items: center
+    margin-bottom: 20px
+
+  &__componente
+    margin-bottom: 20px
+    h1
+      line-height: 1.1em
+
+      @media (max-width: $bp-max-xs)
+        font-size: 2em
+
+  &__descripcion
+    margin-bottom: 20px
+
+  &__row
+    @if $banner-principal-img-x == 'derecha'
+      justify-content: space-between
+    @else
+      justify-content: space-around
+      .banner-principal__img
+        padding-right: 1.5rem!important
+      @media (min-width: $bp-min-sm)
+        .banner-principal__img
+          padding-right: 3rem!important
+
+  &__img
+    @if $banner-principal-img-y == 'arriba'
+      align-self: flex-start
+      padding-bottom: 1.5rem
+      @media (min-width: $bp-min-sm)
+        padding-bottom: 3rem!important
+    @else if $banner-principal-img-y == 'abajo'
+      align-self: flex-end
+      padding-top: 1.5rem
+      @media (min-width: $bp-min-sm)
+        padding-top: 3rem!important
+    @else
+      align-self: center
+      padding-top: 1.5rem
+      padding-bottom: 1.5rem
+      @media (min-width: $bp-min-sm)
+        padding-top: 3rem!important
+        padding-bottom: 3rem!important
+.contenedor-imagenes
+  position: relative
+.imagen_flotante
+  &_1
+    animation: float 6s ease-in-out infinite
+    position: absolute
+    top: 45px;
+    left: 0px;
+    width: 99%;
+
+  &_2
+    animation: float2 6s ease-in-out infinite
+    top: 45px
+    left: 0px
+    position: absolute
+
+@keyframes float
+	0%
+		transform: translatey(0px)
+
+	50%
+		transform: translatey(-20px)
+
+	100%
+		transform: translatey(s0px)
+@keyframes float2
+	0%
+		transform: translatey(-20px)
+
+	50%
+		transform: translatey(0px)
+
+	100%
+		transform: translatey(-20px)
+</style>
